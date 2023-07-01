9173a84bd47b3846b85c1e810ee14f8878d250dd
tetianakushniruk <tetiana.kushnirukk@gmail.com>
2023-07-01 15:46:58 +0300

    Add func for generating interview questions

--- ai21_integration/utils.py
+++ ai21_integration/utils.py
@@ -101,3 +101,50 @@ def books(field):
 
     return data
 
+def interview_q(field):
+    response = ai21.Completion.execute(
+        model="j2-mid",
+        prompt=prompts.interview_q_prompt.format(field=field),
+        numResults=1,
+        maxTokens=450,
+        temperature=0.7,
+        topKReturn=0,
+        topP=1,
+        countPenalty={
+            "scale": 0,
+            "applyToNumbers": False,
+            "applyToPunctuations": False,
+            "applyToStopwords": False,
+            "applyToWhitespaces": False,
+            "applyToEmojis": False
+        },
+        frequencyPenalty={
+            "scale": 0,
+            "applyToNumbers": False,
+            "applyToPunctuations": False,
+            "applyToStopwords": False,
+            "applyToWhitespaces": False,
+            "applyToEmojis": False
+        },
+        presencePenalty={
+            "scale": 0,
+            "applyToNumbers": False,
+            "applyToPunctuations": False,
+            "applyToStopwords": False,
+            "applyToWhitespaces": False,
+            "applyToEmojis": False
+        },
+        stopSequences=["##"]
+    )
+
+    data = response['completions'][0]['data']['text']
+    data = json.loads(data)['interview_questions']
+
+    for i, d in enumerate(data):
+        question = d['question']
+        answer = d['answer']
+        print(f'{i + 1}. {question}: {answer}')
+
+    return data
+
+
