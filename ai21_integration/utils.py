163d9f81ffc36eb2e215fb08763814c39f4bda43
tetianakushniruk <tetiana.kushnirukk@gmail.com>
2023-07-01 15:16:17 +0300

    Add func for generating book recommendations

--- ai21_integration/utils.py
+++ ai21_integration/utils.py
@@ -54,3 +54,50 @@ def roadmap(field):
 
     return data
 
+def books(field):
+    response = ai21.Completion.execute(
+        model="j2-mid",
+        prompt=prompts.books_prompt.format(field=field),
+        numResults=1,
+        maxTokens=450,
+        temperature=0.75,
+        topKReturn=0,
+        topP=1,
+        countPenalty={
+            "scale": 0,
+            "applyToNumbers": False,
+            "applyToPunctuations": False,
+            "applyToStopwords": False,
+            "applyToWhitespaces": False,
+            "applyToEmojis": False
+        },
+        frequencyPenalty={
+            "scale": 0,
+            "applyToNumbers": False,
+            "applyToPunctuations": False,
+            "applyToStopwords": False,
+            "applyToWhitespaces": False,
+            "applyToEmojis": False
+        },
+        presencePenalty={
+            "scale": 0,
+            "applyToNumbers": False,
+            "applyToPunctuations": False,
+            "applyToStopwords": False,
+            "applyToWhitespaces": False,
+            "applyToEmojis": False
+        },
+        stopSequences=["##"]
+    )
+
+    data = response['completions'][0]['data']['text']
+    data = json.loads(data)['books']
+
+    for i, d in enumerate(data):
+        name = d['name']
+        author = d['author']
+        desc = d['description']
+        print(f'{i + 1}. "{name}" by {author}: {desc}')
+
+    return data
+
