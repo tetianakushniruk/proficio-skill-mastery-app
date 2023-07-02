c820d8b36f6d4ba62611ae593eed9cff1bc3b53a
tetianakushniruk <tetiana.kushnirukk@gmail.com>
2023-07-02 15:50:00 +0300

    Front and back integration for Tips

--- app.py
+++ app.py
@@ -9,16 +9,19 @@ def home():
     roadmap = ''
     books = ''
     interview_q = ''
+    tip = ''
     if request.method == 'POST':
         query = request.form['q']
         is_valid = utils.is_valid_profession(query)
         roadmap = utils.roadmap(query)
         books = utils.books(query)
         interview_q = utils.interview_q(query)
+        tip = utils.tip(query)
     return render_template("index.html", title="Proficiō: Home",
                            roadmap=roadmap,
                            books=books,
-                           questions=interview_q)
+                           questions=interview_q,
+                           tip=tip)
 
 @app.route('/find_profession')
 def find_profession():
